184a05973a68d8d69fffb6c4c825c4025f748b15
suryo182 <suryo182@gmail.com>
2021-12-05 12:36:53 +0700

    Add header and bg pattern

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,7 +2,34 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme('colors'),
+        primary: 'hsla(215, 20%, 16%, 1)',
+      }),
+      textColor: (theme) => ({
+        ...theme('colors'),
+        primary: 'hsla(215, 20%, 16%, 1)',
+      }),
+      fontFamily: {
+        'open-sans': ['Open Sans', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        volkhov: ['Volkhov', 'sans-serif'],
+      },
+      fontSize: {
+        '4.25rem': '17px',
+      },
+      lineHeight: {
+        '5.75rem': '23px',
+      },
+      borderRadius: {
+        '1.25rem': '5px',
+      },
+      borderColor: (theme) => ({
+        ...theme('colors'),
+        primary: 'hsla(215, 20%, 16%, 1)',
+      }),
+    },
   },
   variants: {
     extend: {},
